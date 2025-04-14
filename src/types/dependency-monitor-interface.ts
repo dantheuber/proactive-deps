f6c711b2380170e4a3ce317c4076edab70406cd5
Dan Essig <dan.essig@gmail.com>
2025-04-13 19:47:19 -0700

    update stop method description to be accurate

--- src/types/dependency-monitor-interface.ts
+++ src/types/dependency-monitor-interface.ts
@@ -35,7 +35,7 @@ export interface DependencyMonitorInterface {
    */
   startDependencyCheckInterval(): void;
   /**
-   * Starts the interval for checking dependencies.
+   * Stops the interval for checking dependencies.
    * @example
    * monitor.startDependencyCheckInterval();
    */
